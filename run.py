f1126b47612bc33703b5bd13bf5f54dd92066da4
Ben Howkins <benhowkins03@hotmail.com>
2023-01-05 16:42:39 +0000

    First commit

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Imports
+from random import randint
+
+
+class Gameboard:
+    """
+    Basics of the game board including creating the board and the ships
+    as well as player inputs
+    """
+    def __init__(self, board):
+        self.board = board
+    
+    def get_letter_to_num():
+        """
+        Converts the letters of the columns into numbers
+        so it can be used in the user input
+        """
+        letter_to_num = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+        return letter_to_num
+    
+    def print_board(self):
+        """
+        Print the board to the user to play the game
+        """
+        print(" A B C D E F")
+        print(" +-+-+-+-+-+")
+        row_num = 1
+        for row in self.board:
+            print("%d|%s|" % (row_num, "|".join(row)))
+            row_num += 1
